f40ccfd3e41397dda0281528b0bfe66ef89cacd0
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2026-01-28 20:27:12 -0700

    fmt: added white space to main.rs

--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,14 @@ fn main() -> Result<()> {
 fn create_logger() -> Result<(), miette::Error> {
     let strategy = choose_base_strategy().unwrap();
     let log_path = strategy.cache_dir().join(env!("CARGO_PKG_NAME"));
+
     fs::create_dir_all(&log_path).into_diagnostic()?;
+
     let _ = WriteLogger::init(
         LevelFilter::max(),
         Config::default(),
         fs::File::create(log_path.join("log.txt")).expect("Failed to create log file: {log_path}"),
     );
+
     Ok(())
 }
